0fba3fbdd9b6f2f73bf4fceea981e6b1c10b0f37
Vidhiagheda1712 <aghedavidhi1712@gmail.com>
2024-09-19 09:06:11 +0530

    19/09/2024

---
+++ Lecture 2/var.js
@@ -0,0 +1,12 @@
+
+var a = 10, b = 20;
+
+
+
+console.log(`A = ${a}`);
+alert(`A = ${a}`);
+document.write(`A = ${a} `);
+console.log(`B = ${b}`);
+alert(`B = ${b}`);
+document.write(`B = ${b}`);
+document.getElementById(`variable`).innerHTML = `Sum = ${a + b}`
---
+++ Lecture 3/operator.js
@@ -0,0 +1,23 @@
+var a = 10, b = 8, c = 5;
+
+
+console.log(a > b && a > c);
+
+
+
+var a = 10, b = 8, c = 5, d = 2;
+
+
+console.log(a > b && a > c && a > d);
+
+
+
+var a = 40, b = 20, c = 30;
+
+
+console.log(a < b && a < c);
+
+var a = 10, b = 10, c = 10;
+
+
+console.log(a <= b && a <= c);
